cec17557655752abab365847feca41d5610ef8c0
JRodDvlpr <Mialienproductions@gmail.com>
2019-12-19 00:00:50 -0500

    Sign up Form

--- africa-market/src/components/signup.js
+++ africa-market/src/components/signup.js
@@ -1,3 +1,58 @@
-import React from 'react';
+import React, { useState } from "react";
+import "./styles.css";
 
-export default function Signup() {}
+const Form = () => {
+  const [userData, setUserData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: ""
+  });
+
+  const updateFormData = event =>
+  setUserData({
+      ...userData,
+      [event.target.name]: event.target.value
+    });
+
+  const { firstName, lastName, email, password } = userData;
+
+  return (
+    <form>
+      <input
+        value={firstName}
+        onChange={e => updateFormData(e)}
+        placeholder="First name"
+        type="text"
+        name="firstName"
+        required
+      />
+      <input
+        value={lastName}
+        onChange={e => updateFormData(e)}
+        placeholder="Last name"
+        type="text"
+        name="lastName"
+        required
+      />
+      <input
+        value={email}
+        onChange={e => updateFormData(e)}
+        placeholder="Email address"
+        type="email"
+        name="email"
+        required
+      />
+      <input
+        value={password}
+        onChange={e => updateFormData(e)}
+        placeholder="Password"
+        type="password"
+        name="password"
+        required
+      />
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
